35c9f17049e25aedef41633c9a48fb94a94784a3
farascade1 <usame4852@gmail.com>
2024-07-08 11:21:28 +0300

    its light project

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+ import React from "react";
+ import ReactDOM from "react-dom/client"
+import Light from "./Components/Light"
+ import "./index.css"
+
+ const root = document.querySelector("#root")
+ ReactDOM.createRoot(root).render(
+   <div>
+    <Light />
+   </div>
+ )
